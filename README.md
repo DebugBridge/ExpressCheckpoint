f4b5757e26ed1dd8a3ca025bdbb8f8eb763750b2
Ubuntu <debug_bridge@outlook.com>
2021-08-13 23:50:51 +0900

    set up config for knex and express. working for grabing all movies.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ExpressCheckpoint
+
+fresh
+
+get movies
+
+As a client application consuming your api application,
+I want to be able to receive a list of movies from the database,
+so that I can list them on my interface.
+
+As a client application consuming your application,
+I want to be able to search by title for movies from the database,
+so that I can list them on my interface.
+
+
---
+++ migrations/20210813232655_movies.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('movies', table => {
+        table.increments('id'); // adds an auto incrementing PK column
+        table.string('title').notNullable();
+        table.integer('runtime');
+        table.integer('release_year');
+        table.string('director'); // adds created_at and updated_at
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('movies');
+};
