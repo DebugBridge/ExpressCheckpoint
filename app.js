4cfd9b3ec72f7b19c02fc3c80abed204c1984512
Ubuntu <debug_bridge@outlook.com>
2021-08-14 03:40:27 +0900

    added serch by movie via ? and tests.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 const knex = require('knex')(require('./knexfile.js')['development']);
 
@@ -12,8 +11,22 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+///handle the query operation here
 app.get('/movies', function(req, res) {
-  knex
+  if(req.query.title){
+    knex
+    .select('*')
+    .from('movies')
+    .where('title', 'ilike',`%${req.query.title}%`)
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'The data you are looking for could not be found. Please try again'
+      })
+    );
+  } else {
+    knex
     .select('*')
     .from('movies')
     .then(data => res.status(200).json(data))
@@ -23,12 +36,63 @@ app.get('/movies', function(req, res) {
           'The data you are looking for could not be found. Please try again'
       })
     );
+  }
 });
+
+app.get('/movies/:id', function(req, res){
+    /* GET a movie by id */
+
+    knex
+    .select('*')
+    .from('movies')
+    .where('id', req.params.id)
+    .then(data => {
+        if (data.length === 0){
+          return res.status(404).json({
+            message:
+            'Movie ID not found'
+          })
+        } else {
+          return res.status(200).json(data)
+        }
+      })
+      .catch(err =>
+        res.status(400).json({
+          message:
+            'Invalid ID supplied'
+        })
+      );
+})
+
+// app.get('/movies?title={titleQuery}', function(req, res){
+//   /* GET a movie by title */
+//   console.log('req params', req.params);
+//   knex
+//   .select('*')
+//   .from('movies')
+//   .where('title', req.query.title)
+//   .then(data => {
+//       if (data.length === 0){
+//         return res.status(404).json({
+//           message:
+//           'Movie ID not found'
+//         })
+//       } else {
+//         return res.status(200).json(data)
+//       }
+//     })
+//     .catch(err =>
+//       res.status(400).json({
+//         message:
+//           'Invalid ID supplied'
+//       })
+//     );
+// })
 
 // app.post()
 
 // app.delete()
 
-app.listen(PORT, () => {
-  console.log(`The server is running on http://localhost:${PORT}`);
-});
+
+module.exports = app;
+
---
+++ app.test.js
@@ -0,0 +1,82 @@
+const { response } = require('express');
+const request = require('supertest');
+const app = require('./app.js');
+
+describe('root route', () => {
+    it('returns all movies in list on /movies', (done) => {
+        request(app)
+        .get('/movies')
+        .then((response) => {
+            expect(response.status).toBe(200)
+            expect(response.type).toBe('application/json');
+            done();
+        })
+    });
+
+    it('Should return a single entry from the database on /movies/:id where id = 1', (done) => {
+        let returnedMovie1 = [{"id": 1, "title": "Midnight In Paris","runtime": 96,"release_year": 2011,"director": "Woody Allen"}];
+        let returnedMovie2 = [{"id": 2, "title": "Titanic","runtime": 210, "release_year": 1997,"director": "James Cameron"}];
+        let returnedMovie3 = [{"id": 3, "title": "From Paris With Love","runtime": 94,"release_year": 2010,"director": "Pierre Morel"}];
+
+        request(app)
+        .get('/movies/1')
+        .then((response) => {
+            expect(response.status).toBe(200)
+            expect(response.type).toBe('application/json')
+            expect(response.body).toEqual(returnedMovie1)
+            done();
+        })
+        request(app)
+        .get('/movies/2')
+        .then((response) => {
+            expect(response.status).toBe(200)
+            expect(response.type).toBe('application/json')
+            expect(response.body).toEqual(returnedMovie2)
+            done()
+        })
+        request(app)
+        .get('/movies/3')
+        .then((response) => {
+            expect(response.status).toBe(200)
+            expect(response.type).toBe('application/json')
+            expect(response.body).toEqual(returnedMovie3)
+            done()
+        })
+    });
+
+    it("returns 'Movie ID not found' & '404' status code when visiting '/movies/420' ", (done) => {
+        request(app)
+        .get('/movies/420')
+        .then((response) => {
+            expect(response.status).toBe(404)
+            expect(response.type).toBe('application/json')
+            expect(response.body).toEqual({"message":"Movie ID not found"})
+            done();
+        })
+    });
+
+    it("returns 'Invalid ID supplied' & '400' status code when visiting '/movies/foobar' ", (done) => {
+        request(app)
+        .get('/movies/foobar')
+        .then((response) => {
+            expect(response.status).toBe(400)
+            expect(response.type).toBe('application/json')
+            expect(response.body).toEqual({"message":"Invalid ID supplied"})
+            done();
+        })
+    });
+
+    it("Should return entries based on the search query when visiting /movies?title=midnight.", (done) => {
+        let returnedMovie1 = [{"id": 1, "title": "Midnight In Paris","runtime": 96,"release_year": 2011,"director": "Woody Allen"}];
+
+        request(app)
+        .get('/movies?title=midnight')
+        .then((response) => {
+            expect(response.status).toBe(200)
+            expect(response.type).toBe('application/json')
+            expect(response.body).toEqual(returnedMovie1)
+            done();
+        })
+    });
+
+})
